b973608df3d16aed773cc78c165fe2c9612c3590
vespie2709 <vespie@ensc.fr>
2023-03-16 09:27:04 +0100

    début

--- App.js
+++ App.js
@@ -1,14 +1,45 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { FlatList, StyleSheet, Text, View } from 'react-native';
+//import firebase from 'firebase/app';
+//import 'firebase/firestore';
+//import 'firebase/auth';
+import { getDocs } from "firebase/firestore";
+import {utilisateurCollection} from 'firebase';
+//import {casiercollection} from 'firebase';
 
 export default function App() {
+  const [utilisateur, setUsers] = useState([]);
+
+  const fetchUsers = async () => {
+    const userList = []; // je créer une liste vide
+    const querySnapshot = await getDocs(utilisateurCollection);
+    querySnapshot.forEach((doc) => { // chaque élément est ajouté à la liste
+      userList.push({ id: doc.id, ...doc.data() });
+    });
+    setUsers(userList); // je stocke ma liste dans l'état de l'application
+  };
+  useEffect(() => {
+    fetchUsers();
+  }, []);
+}
+{userList.map((user) => (
+  <Text key={user.id}>
+    id: {user.id} - name: {user.name} - email: {user.email},
+  </Text>
+))}
+
+/*export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <FlatList>
+        data={utilisateur}
+        renderItem={({ item }) => <Text style={styles.row}>{item}</Text>}
+        keyExtractor={(item) => item}
+      </FlatList>
       <StatusBar style="auto" />
     </View>
   );
-}
+} */
 
 const styles = StyleSheet.create({
   container: {
---
+++ firebase.js
@@ -0,0 +1,31 @@
+// Import the functions you need from the SDKs you need
+//import firebase from 'firebase/app';
+//import 'firebase/firestore';
+//import 'firebase/auth';
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getFirestore, collection } from "firebase/firestore";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyA98BL0l6hSbX5vOXkHwqKU8UkGLaZsd_I",
+  authDomain: "easylock-f24e7.firebaseapp.com",
+  databaseURL: "https://easylock-f24e7-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "easylock-f24e7",
+  storageBucket: "easylock-f24e7.appspot.com",
+  messagingSenderId: "212728487425",
+  appId: "1:212728487425:web:88ea2fbc4789338f6e2a00",
+  measurementId: "G-WPT428DTML"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
+export const db = getFirestore(app);
+
+export const utilisateurCollection = collection(db, "utilisateur");
+export const casierCollection = collection(db, "casier");
